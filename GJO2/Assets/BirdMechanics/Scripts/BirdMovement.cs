5e90652d4652773f31b5104dfa4bf1471085ded3
Pulpo <pol.gomar@enti.cat>
2019-10-23 13:05:42 +0200

    Bird Basic Movement

---
+++ GJO2/Assets/BirdMechanics/Scripts/BirdMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdMovement : MonoBehaviour
+{
+
+    public Rigidbody J1RB;
+    public Rigidbody J2RB;
+    public Rigidbody J3RB;
+    public Rigidbody J4RB;
+
+    void Start() {
+        
+    }
+
+    void BasicMovement(string Horizontal, string Vertical, Rigidbody Player) {
+        float speed = 4.5f;
+        float HorizontalMovement = speed * Input.GetAxis(Horizontal);
+        float VerticalMovement = speed * Input.GetAxis(Vertical);
+
+        Player.velocity = new Vector3(HorizontalMovement * speed, Player.velocity.y);
+        Player.velocity = new Vector3(Player.velocity.x, VerticalMovement * speed);
+    }
+
+    //void J2Movement() {
+    //    float speed = 4.5f;
+    //    float HorizontalMovement = speed * Input.GetAxis("J2Horizontal");
+    //    float VerticalMovement = speed * Input.GetAxis("J2Vertical");
+
+    //    J2RB.velocity = new Vector3(HorizontalMovement * speed, J2RB.velocity.y);
+    //    J2RB.velocity = new Vector3(J2RB.velocity.x, VerticalMovement * speed);
+    //}
+
+
+    // Update is called once per frame
+    void Update() {
+        BasicMovement("J1Horizontal", "J1Vertical", J1RB);
+        BasicMovement("J2Horizontal", "J2Vertical", J2RB);
+        //J2Movement();
+
+    }
+}
